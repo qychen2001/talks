f0238ad82ed8ac8f43a1ddf8d2b5157ae6a236d3
qychen2001 <72334646+qychen2001@users.noreply.github.com>
2024-08-21 00:16:53 +0000

    Deploying to gh-pages from @ qychen2001/slides@aa1c1a3dc0f0dd7a214c19d5fab7d74479c94c01 🚀

---
+++ test2/assets/slidev/default-C8qKunkN.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-BG0mrsKf.js";import{_ as r}from"../index-CJT-xyff.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
